dec1aa1e88b8abd8720667c74ee9684b51899de3
lesley <lesleybanadzem2017@gmail.com>
2023-12-30 16:07:04 -0600

    Refactored index to a class.

--- index.js
+++ index.js
@@ -1,103 +1,95 @@
 import { createWorker } from "tesseract.js";
+import SpotifyWebApi from "spotify-web-api-node";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const screenshotDir = __dirname + "/screenshots";
-
-const screenshotFileNames = fs.readdirSync(screenshotDir).sort((a, b) => (a > b ? 1 : -1));
-
-// Sort the file names so that they are in order
-
-/**
- * @description
- * 		1. create an output files
- * 		2. create a tesseract worker
- * 		3. for each screenshot file name
- * 			- recognize the text in the image
- * 			- write the text to the output file
- * 		4. close the worker
- */
-(async () => {
-	fs.createWriteStream("output.txt");
-	const worker = await createWorker("eng");
-	for (const screenshotFileName of screenshotFileNames) {
-		const pathToScreenshot = screenshotDir + "/" + screenshotFileName;
-		try {
-			const ret = await worker.recognize(pathToScreenshot);
-			const textData = ret.data.text;
-			console.log("Writing to output data", screenshotFileName);
-			const pathToOutputFile = __dirname + "/output.txt";
-			fs.appendFile(pathToOutputFile, textData + "\n", (e) => {
-				if (e) console.log(e);
-			});
-			console.log("Done Writing to output data", screenshotFileName);
-		} catch (e) {
-			console.log("Error at Image path,", pathToScreenshot);
-			console.log(e);
-		}
+class ShazamTrackExtractor {
+	constructor() {
+		this.__dirname = path.dirname(fileURLToPath(import.meta.url));
+		this.screenshotDir = this.__dirname + "/screenshots";
+		this.screenshotFileNames = fs
+			.readdirSync(this.screenshotDir)
+			.sort((a, b) => (a > b ? 1 : -1));
+		this.pathToOutputFile = this.__dirname + "/output.txt";
+		this.outputJson = "shazamOutput.json";
+		this.regExpToMatchMonthNameAbbr =
+			/\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s(0[1-9]|[12][0-9]|3[01]),\s\d{4}\b/g;
 	}
-	await worker.terminate();
-})();
 
-const pathToOutputFile = __dirname + "/output.txt";
-const outputJson = "shazamOutput.json";
-fs.createWriteStream(outputJson, "");
+	async extractTextFromImages() {
+		fs.createWriteStream("output.txt");
+		const worker = await createWorker("eng");
+		for (const screenshotFileName of this.screenshotFileNames) {
+			const pathToScreenshot = this.screenshotDir + "/" + screenshotFileName;
+			try {
+				const ret = await worker.recognize(pathToScreenshot);
+				const textData = ret.data.text;
+				console.log("Writing to output data", screenshotFileName);
+				fs.appendFile(this.pathToOutputFile, textData + "\n", (e) => {
+					if (e) console.log(e);
+				});
+				console.log("Done Writing to output data", screenshotFileName);
+			} catch (e) {
+				console.log("Error at Image path,", pathToScreenshot);
+				console.log(e);
+			}
+		}
+		await worker.terminate();
+	}
 
-/**
- * @description
- * 1. read the output file
- * 2. split the data by new line
- * 3. remove empty strings
- * 4. return the array
- * @returns {string[]} fileDataArray
- */
-const readOutputFile = () => {
-	const fileData = fs.readFileSync(pathToOutputFile, "utf8");
-	const fileDataArray = fileData.split("\n").filter(Boolean);
+	readOutputFile() {
+		const fileData = fs.readFileSync(this.pathToOutputFile, "utf8");
+		const fileDataArray = fileData.split("\n").filter(Boolean);
 
-	fs.appendFile(outputJson, JSON.stringify(fileDataArray), (err) => {
-		if (err) console.log(err);
-	});
+		fs.appendFile(
+			this.outputJson,
+			JSON.stringify(fileDataArray),
+			(err) => err && console.log(err)
+		);
 
-	return fileDataArray;
-};
+		return fileDataArray;
+	}
 
-const fileDataArray = readOutputFile();
-// If string begins with a month abbr and the next two elements are between 1-31, then it is a date
+	extractShazamTracks() {
+		const fileDataArray = this.readOutputFile();
+		const n = fileDataArray.length;
+		const seen = new Set();
+		const shazamTracks = [];
 
-const regExpToMatchMonthNameAbbr =
-	/\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s(0[1-9]|[12][0-9]|3[01]),\s\d{4}\b/g;
+		for (let i = 0; i < n; i++) {
+			const element = fileDataArray[i];
+			const titleWithArtist = fileDataArray[i - 2] + " " + fileDataArray[i - 1];
 
-const n = fileDataArray.length;
-const seen = new Set();
-const shazamTracks = [];
+			if (seen.has(titleWithArtist)) continue;
 
-for (let i = 0; i < n; i++) {
-	const element = fileDataArray[i];
-	const titleWithArtist = fileDataArray[i - 2] + " " + fileDataArray[i - 1];
+			const matches = element.match(this.regExpToMatchMonthNameAbbr);
 
-	if (seen.has(titleWithArtist)) continue;
+			if (matches?.length) {
+				shazamTracks.push({
+					track: fileDataArray[i - 2],
+					artist: fileDataArray[i - 1],
+					q: `remaster%2520track:${encodeURI(
+						fileDataArray[i - 2]
+					)}%2520artist:${encodeURI(fileDataArray[i - 1])}`,
+					date: matches[0],
+				});
+				seen.add(titleWithArtist);
+			}
+		}
 
-	const matches = element.match(regExpToMatchMonthNameAbbr);
+		fs.createWriteStream("shazamTracks.json", "");
 
-	if (matches?.length) {
-		shazamTracks.push({
-			track: fileDataArray[i - 2],
-			artist: fileDataArray[i - 1],
-			q: `remaster%2520track:${encodeURI(fileDataArray[i - 2])}%2520artist:${encodeURI(
-				fileDataArray[i - 1]
-			)}`,
-			date: matches[0],
+		fs.appendFile("shazamTracks.json", JSON.stringify(shazamTracks), (err) => {
+			if (err) console.log(err);
 		});
-		seen.add(titleWithArtist);
+	}
+	async run() {
+		// await this.extractTextFromImages();
+		this.extractShazamTracks();
 	}
 }
 
-fs.createWriteStream("shazamTracks.json", "");
+const extractor = new ShazamTrackExtractor();
 
-fs.appendFile("shazamTracks.json", JSON.stringify(shazamTracks), (err) => {
-	if (err) console.log(err);
-});
+extractor.run();
